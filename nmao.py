6e831acbacdd10c7533adcae072f09dc18078ad0
sajjad_salam <wtgd5777@gmail.com>
2023-02-25 15:45:23 +0300

    scan network

--- nmao.py
+++ nmao.py
@@ -5,3 +5,16 @@ nm =nmap.PortScanner()
 
 scanrange=nm.scan(hosts="192.168.1.100-105")
 pprint.pprint(scanrange["scan"])
+
+# ==========================
+def scan_network():
+    nm = nmap.PortScanner()
+    scan_range = nm.scan(hosts="192.168.0.104")
+    data = scan_range['scan']
+    for value in data.values():
+        print(value['addresses'])
+        print('status: ', value['status']['state'])
+        print(value['tcp'])
+
+
+scan_network()
