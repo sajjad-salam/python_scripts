730b2417f0fa12827579d8ca97032cf5b3b36082
sajjad-salam <sajjad.salam.teama@gmail.com>
2023-08-20 12:25:29 +0300

    new

--- vodu/esp.py
+++ vodu/esp.py
@@ -1,26 +1,23 @@
 import re
 import os
 import requests
-from tqdm import tqdm  # Import tqdm library
+from tqdm import tqdm 
 
-# Sample text containing video links
 sample_text = """
 html code 
 """
 
-# Regular expression pattern to match video URLs
+
+
 video_url_pattern = r"https://\S+\.mp4"
+
 series_name="The_Walking_Dead"
-# Find all video matches in the sample text
+
 video_matches = re.findall(video_url_pattern, sample_text)
 
-# Specify the base directory for saving videos
 base_directory = r"C:\Users\wtgd5\Desktop\Video\Walking Dead"
 
-# Create the base directory if it doesn't exist
 os.makedirs(base_directory, exist_ok=True)
-
-# Download and save videos to the specified path with progress bar
 for video_link in tqdm(video_matches, desc="Downloading Videos"):
     video_filename = os.path.basename(video_link)
     video_save_path = os.path.join(base_directory, video_filename)
--- vodu/subtitle.py
+++ vodu/subtitle.py
@@ -2,25 +2,19 @@ import re
 import os
 import requests
 
-# Sample text containing subtitle links
 sample_text = """
 html code
 """
 
-# Regular expression pattern to match subtitle URLs
 subtitle_url_pattern = r"https://movie\.vodu\.me/subtitles/.*?_S(\d+)E(\d+)_(\d+)\.srt"
 series_name="The_Walking_Dead"
 
-# Find all subtitle matches in the sample text
 subtitle_matches = re.findall(subtitle_url_pattern, sample_text)
 
-# Specify the base directory for saving subtitles
 base_directory = r"C:\Users\wtgd5\Desktop\Video\Walking-Dead"
 
-# Create the base directory if it doesn't exist
 os.makedirs(base_directory, exist_ok=True)
 
-# Download and save subtitles to the specified path
 for season_number, episode_number, subtitle_number in subtitle_matches:
     subtitle_filename = f"{series_name}_S{season_number}E{episode_number}.srt"
     subtitle_save_path = os.path.join(base_directory, subtitle_filename)
